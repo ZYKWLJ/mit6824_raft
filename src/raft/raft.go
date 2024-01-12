dd7652d4b2ebaf38b21611c245b201562c5c07cb
songpengwei <songpengwei40@gmail.com>
2024-01-12 23:39:37 +0800

    reset timer when become candidate

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -128,6 +128,7 @@ func (rf *Raft) becomeCandidateLocked() {
 	}
 
 	LOG(rf.me, rf.currentTerm, DVote, "%s->Candidate, For T%d", rf.role, rf.currentTerm+1)
+	rf.resetElectionTimerLocked()
 	rf.currentTerm++
 	rf.role = Candidate
 	rf.votedFor = rf.me
